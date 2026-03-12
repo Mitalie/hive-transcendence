02727381e7f042c82c0b90ce7bc7f9d18369bb46
Dimitris <dvlachos@student.hive.fi>
2026-03-12 10:37:24 +0200

    platform skeleton with pages + navbar

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Link from "next/link";
+import styles from "./NavBar.module.css";
+
+export default function NavBar() {
+  return (
+    <nav className={styles.nav}>
+      <Link href="/login">Login</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/leaderboard">Leaderboard</Link>
+      <Link href="/game">Game</Link>
+    </nav>
+  );
+}
---
+++ src/app/leaderboard/page.tsx
@@ -0,0 +1,8 @@
+export default function LeaderboardPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Leaderboard</h1>
+      <p>Match scores and rankings will go here.</p>
+    </div>
+  );
+}
