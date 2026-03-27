8a3d0cbc8f9cf54196a907cfb8acf3010303543f
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2026-03-27 18:48:40 +0200

    Update src/i18n/i18n.ts
    Co-authored-by: Anssi Mäkinen <16635602+Mitalie@users.noreply.github.com>

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -4,17 +4,15 @@ import en from "./locales/en.json";
 import fi from "./locales/fi.json";
 import zh from "./locales/zh.json";
 
-if (!i18n.isInitialized) {
-  i18n.use(initReactI18next).init({
-    resources: {
-      en: { translation: en },
-      fi: { translation: fi },
-      zh: { translation: zh },
-    },
-    fallbackLng: "en",
-    interpolation: { escapeValue: false },
-    react: { useSuspense: false },
-  });
-}
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    fi: { translation: fi },
+    zh: { translation: zh },
+  },
+  fallbackLng: "en",
+  interpolation: { escapeValue: false },
+  react: { useSuspense: false },
+});
 
 export default i18n;
