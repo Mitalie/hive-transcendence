2cd09766c1322c0ae927966cbce5bbc66081049f
Dimitris <dvlachos@student.hive.fi>
2026-03-31 15:50:57 +0300

    Implement match history storage (#32)
    * Add Match model to database schema
    * Add NextAuth hooks and type declarations for making user ID available through session
    * Add server data layer functions for getting and storing matches
    * Add server action for storing matches and call it from frontend

---
+++ src/actions/matchHistory.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { SaveMatchArgs, saveMatch } from "@/data/matchHistory";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+function validateSaveMatchArgs(o: unknown): o is SaveMatchArgs {
+  if (typeof o !== "object" || o === null) return false;
+  if (!("player2" in o) || typeof o.player2 !== "string") return false;
+  if (!("score1" in o) || typeof o.score1 !== "number") return false;
+  if (!("score2" in o) || typeof o.score2 !== "number") return false;
+  return true;
+}
+
+export async function saveMatchAction(args: SaveMatchArgs) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) throw new Error("Unauthorized");
+
+  if (!validateSaveMatchArgs(args))
+    throw new Error("Invalid arguments to action");
+
+  saveMatch(session.user.id, args);
+}
