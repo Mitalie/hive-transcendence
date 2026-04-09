9365e3219b40bb855760dd44e0001c82177534d2
Anssi Mäkinen <amakinen@student.hive.fi>
2026-04-09 19:12:27 +0300

    Add simple start and end screens to game

--- src/game/Game.tsx
+++ src/game/Game.tsx
@@ -4,6 +4,7 @@ import { useCallback, useEffect } from "react";
 import GameRender from "@/game/3d/GameRender";
 import GameUI from "@/game/ui/GameUI";
 import {
+  exitConfirmAction,
   pauseAction,
   resumeAction,
   scoreP1Action,
@@ -11,6 +12,8 @@ import {
   startGameAction,
   useGameState,
 } from "@/game/GameState";
+import Button from "@/components/Button";
+import ScoreBoard from "./ui/ScoreBoard";
 
 export default function Game() {
   const [state, dispatch] = useGameState();
@@ -35,6 +38,47 @@ export default function Game() {
     window.addEventListener("keydown", handleKeyDown);
     return () => window.removeEventListener("keydown", handleKeyDown);
   }, [dispatch, state.paused, state.view, state.exitPromptOpen]);
+
+  const onStartClick = useCallback(() => {
+    dispatch(startGameAction());
+  }, [dispatch]);
+
+  const onExitClick = useCallback(() => {
+    dispatch(exitConfirmAction());
+  }, [dispatch]);
+
+  if (state.view === "start") {
+    return (
+      <div className="relative h-full w-full rounded-xl bg-[#050505] grid">
+        <Button
+          className="bg-btn-purple hover:bg-btn-purple-hover m-auto"
+          onClick={onStartClick}
+        >
+          Start Game
+        </Button>
+      </div>
+    );
+  }
+
+  if (state.view === "end") {
+    return (
+      <div className="relative h-full w-full rounded-xl bg-[#050505] grid">
+        <ScoreBoard p1={state.score1} p2={state.score2} />
+        <Button
+          className="bg-btn-purple hover:bg-btn-purple-hover m-auto"
+          onClick={onStartClick}
+        >
+          Play Again
+        </Button>
+        <Button
+          className="bg-btn-blue hover:bg-btn-blue-hover m-auto"
+          onClick={onExitClick}
+        >
+          Return to Menu
+        </Button>
+      </div>
+    );
+  }
 
   return (
     <div className="relative h-full w-full">
