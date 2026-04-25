730a1aac6a64ae6c6acc758ec3363670891bc4ec
Anssi Mäkinen <amakinen@student.hive.fi>
2026-04-25 14:48:59 +0300

    SessionGuard: don't call signOut if already not authenticated
    Also, there is no need for this component to wrap children.

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,19 +4,18 @@ import { SessionProvider, useSession, signOut } from "next-auth/react";
 import { I18nextProvider } from "react-i18next";
 import { createI18n } from "@/i18n/i18n";
 
-function SessionGuard({ children }: { children: React.ReactNode }) {
-  const { data: session, status } = useSession();
+function SessionGuard() {
+  const { data: session } = useSession();
 
   useEffect(() => {
-    if (
-      status === "unauthenticated" ||
-      (status === "authenticated" && !session?.user)
-    ) {
+    // The session is invalid if server doesn't recognize the user.
+    // Clear out the useless session cookie to avoid confusion.
+    if (session && !session.user) {
       signOut({ redirect: false });
     }
-  }, [status, session]);
+  }, [session]);
 
-  return <>{children}</>;
+  return null;
 }
 
 export function Providers({
@@ -30,9 +29,8 @@ export function Providers({
 
   return (
     <SessionProvider>
-      <SessionGuard>
-        <I18nextProvider i18n={i18nInstance}>{children}</I18nextProvider>
-      </SessionGuard>
+      <SessionGuard />
+      <I18nextProvider i18n={i18nInstance}>{children}</I18nextProvider>
     </SessionProvider>
   );
 }
